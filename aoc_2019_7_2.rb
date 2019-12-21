56d1ae68bdd77fe3172f6fb74730a30e1809ae78
Dennis Gokman <dennis.gokman@gmail.com>
2019-12-21 14:29:29 -0500

    solved advent of code day 7 part 2

--- aoc_2019_7_2.rb
+++ aoc_2019_7_2.rb
@@ -6,10 +6,11 @@ def value(mode, arr, i)
   end
 end    
 
-def run(arr,input1,input2)
-  i = 0
-  output = 0
+def run(arr,input1,input2,idx,looped)
+  i = idx
+  output = nil
   input_one_done = false
+  input_two_done = false
   loop do
     opcode, modes = parameters(arr[i])
     if opcode == 1
@@ -19,25 +20,26 @@ def run(arr,input1,input2)
       arr[arr[i+3]] = value(modes[0].to_i, arr, i+1) * value(modes[1].to_i, arr, i+2)
       i += 4
     elsif opcode == 3
-      if !input_one_done
+      if !input_one_done && !looped
         arr[arr[i+1]] = input1 
         input_one_done = true
       else
         arr[arr[i+1]] = input2
-      end
+      end  
       i += 2
     elsif opcode == 4
       output = value(modes[0].to_i, arr, i+1)
+      return [output,i+2]
       i += 2
     elsif opcode == 5
       if value(modes[0].to_i, arr, i+1) != 0
-        i = arr[i+2] 
+        i = value(modes[1].to_i, arr, i+2)
       else
         i += 3
       end  
     elsif opcode == 6
       if value(modes[0].to_i, arr, i+1) == 0
-        i = arr[i+2] 
+        i = value(modes[1].to_i, arr, i+2)
       else
         i += 3 
       end  
@@ -60,9 +62,7 @@ def run(arr,input1,input2)
       end
       i += 4      
     elsif opcode == 99
-      return output
-    else
-      i = arr[i]
+      return [output, nil]
     end   
   end  
 end
@@ -78,14 +78,37 @@ A = File.read("aoc_2019_7.txt")
 arr = A.split(",").map(&:to_i)
 
 max = 0
-(0..4).to_a.permutation(5).each do |inputs|
+looped = false
+input2 = 0
+output = 0
+[5,6,7,8,9].permutation(5).each do |inputs|
+  looped = false
   input2 = 0
-  inputs.each do |input1|
-    input2 = run(arr,input1,input2)
-    arr = A.split(",").map(&:to_i) 
-  end
-  if input2 > max
-    max = input2
+  output = 0
+  arr0 = A.split(",").map(&:to_i)
+  arr1 = A.split(",").map(&:to_i)
+  arr2 = A.split(",").map(&:to_i)
+  arr3 = A.split(",").map(&:to_i)
+  arr4 = A.split(",").map(&:to_i)
+  idxs = [0,0,0,0,0]
+  loop do
+    new_inputs = inputs.clone
+    inputs.each_with_index do |input1,idx|
+      next unless input1
+      input2, i = run(eval("arr#{idx}"),input1,input2,idxs[idx],looped)
+      if i.nil?
+        new_inputs[idx] = nil
+      else  
+        idxs[idx] = i
+      end
+    end
+    output = input2 if input2
+    looped = true
+    inputs = new_inputs.clone
+    break if !inputs.any?
+  end  
+  if output > max
+    max = output
   end
 end
 
